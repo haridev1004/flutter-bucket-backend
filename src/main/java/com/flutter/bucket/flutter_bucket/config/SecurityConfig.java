0246b99aed93b8f87fef7dbfd6cfbb8d4113050f
Hari Dev <hariravi.dev.1004@gmail.com>
2025-06-23 16:39:23 +0530

    fix

--- src/main/java/com/flutter/bucket/flutter_bucket/config/SecurityConfig.java
+++ src/main/java/com/flutter/bucket/flutter_bucket/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.flutter.bucket.flutter_bucket.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +23,18 @@ public class SecurityConfig {
                 .anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://13.48.147.138:8080")); // frontend origin
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
