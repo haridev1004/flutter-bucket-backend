477a409c1c2570cbc3fd0f644d3186c8ffe43e99
Hari Dev <hariravi.dev.1004@gmail.com>
2025-06-23 16:46:29 +0530

    fix

--- src/main/java/com/flutter/bucket/flutter_bucket/config/SecurityConfig.java
+++ src/main/java/com/flutter/bucket/flutter_bucket/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080", "http://13.48.147.138:8080", "*")); // frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:8080", "http://13.48.147.138:8080")); // frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
